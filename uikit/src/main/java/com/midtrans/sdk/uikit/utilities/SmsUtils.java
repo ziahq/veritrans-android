7ff0344a0a41d43fd5f9ddb3bb2bc53097c0daa2
rakawestu <rakamogandhi@hotmail.com>
2016-12-09 14:40:51 +0700

    Add utils to do SMS related function

---
+++ uikit/src/main/java/com/midtrans/sdk/uikit/utilities/SmsUtils.java
@@ -0,0 +1,48 @@
+package com.midtrans.sdk.uikit.utilities;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by rakawm on 12/9/16.
+ */
+
+public class SmsUtils {
+    public static final Pattern ALL_PATTERN = Pattern.compile("CIMB Niaga: Paycode Anda adalah [0-9]+ utk transaksi di [a-zA-Z0-9 ]+ sebesar IDR [0-9.,]+. Paycode berlaku selama [0-9]+ mnt.|From BCA: Your Authorisation code is [0-9]+ for the purchase at [a-zA-Z0-9 ]+, amount IDR [0-9.,]+. Valid for [0-9]+ mins.|Dari BNI: Kode Otorisasi utk transaksi Anda adalah [0-9]+ di [a-zA-Z0-9 ]+ sebesar IDR [0-9.,]+.Kode Anda berlaku [0-9]+ mnt.");
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("[0-9]{6}");
+    private static final Pattern BCA_PATTERN = Pattern.compile("From BCA: Your Authorisation code is [0-9]+ for the purchase at [a-zA-Z0-9 ]+, amount IDR [0-9.,]+. Valid for [0-9]+ mins.");
+    private static final Pattern BNI_PATTERN = Pattern.compile("Dari BNI: Kode Otorisasi utk transaksi Anda adalah [0-9]+ di [a-zA-Z0-9 ]+ sebesar IDR [0-9.,]+.Kode Anda berlaku [0-9]+ mnt.");
+    private static final Pattern CIMB_PATTERN = Pattern.compile("CIMB Niaga: Paycode Anda adalah [0-9]+ utk transaksi di [a-zA-Z0-9 ]+ sebesar IDR [0-9.,]+. Paycode berlaku selama [0-9]+ mnt.");
+
+    /**
+     * Check if the message is an OTP message by using regex pattern.
+     */
+    public static boolean isOtpMessage(String message) {
+        return ALL_PATTERN.matcher(message).matches();
+    }
+
+    /**
+     * Get payment code from SMS message.
+     *
+     * @param message SMS message
+     */
+    public static String getCodeFromMessage(String message) {
+        if (BCA_PATTERN.matcher(message).matches()) {
+            Matcher bcaMatcher = NUMBER_PATTERN.matcher(message);
+            return extractCodeFromMessage(bcaMatcher);
+        } else if (BNI_PATTERN.matcher(message).matches()) {
+            Matcher bniMatcher = NUMBER_PATTERN.matcher(message);
+            return extractCodeFromMessage(bniMatcher);
+        } else if (CIMB_PATTERN.matcher(message).matches()) {
+            Matcher cimbMatcher = NUMBER_PATTERN.matcher(message);
+            return extractCodeFromMessage(cimbMatcher);
+        }
+
+        return null;
+    }
+
+    private static String extractCodeFromMessage(Matcher matcher) {
+        matcher.find();
+        return matcher.group(0);
+    }
+}
